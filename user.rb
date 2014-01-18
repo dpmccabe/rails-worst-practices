98abda2e9814eb2d0b498466606209da8ee2f660
Devin McCabe <devin@sandboxindustries.com>
2014-01-17 19:02:53 -0600

    initial

---
+++ user.rb
@@ -0,0 +1,78 @@
+class User < ActiveRecord::Base
+
+  MAILCHIMP_API_KEY = 'm23lm092m3'
+
+  has_many :orders
+  has_many :packages, through: :orders
+
+  before_save :assign_referral_code
+  after_create :schedule_welcome_email
+  
+  validates_presence_of :email, :fname, :lname
+  validates :referral_code, uniqueness: true
+
+  scope :recently_created, where("created_at <= #{Date.today - 2.days}")
+
+  def name
+    self.fname + ' ' + self.lname
+  end
+
+  def formatted_name
+    "<strong>#{name}</strong> (<a href=\"mailto:#{email}\">#{email}</a>)"
+  end
+
+  def assign_referral_code
+    referral_code = SecureRandom.hex(6)
+  end
+
+  def schedule_welcome_email
+    Mailer.delay.welcome(self) # Queue email with DelayedJob
+  end
+
+  def has_orders
+    orders.any?
+  end
+
+  def most_recent_package
+    order.last.package
+  end
+
+  def can_manage?
+    (email = 'manager@example.com') or (email = 'admin@example.com')
+  end
+
+  def order_product(product)
+    result = OrderProcessor.charge(self, product)
+
+    if result
+      Event.log_order_processing(self)
+      Mailer.order_confirmation(self, product).deliver
+      return true
+    else
+      Event.log_failed_order_processing(self)
+      return false
+    end
+  end
+
+  def self.delete_user(email)
+    begin
+      user = User.find_by_email(email)
+      user.destroy!
+    rescue Exception => e # email not found
+      Rails.logger.error("Could not delete user with email #{email}")
+    end
+  end
+
+end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                        :integer          not null, primary key
+#  email                     :string(255)      default(""), not null
+#  fname                     :string(255)      default(""), not null
+#  lname                     :string(255)      default(""), not null
+#  referral_code             :string(255)
+#  created_at                :datetime
+#  updated_at                :datetime
---
+++ user_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe User do
+  it 'I can create a new user' do
+    user = User.create(email: 'test@example.com', fname: 'John', lname: 'Doe')
+    expect(user.id).to_not be_nil
+  end
+
+  it 'validates the required fields' do
+    user = User.new(email: nil, fname: nil, lname: nil)
+    user.errors[:email].should include('is required')
+    user.errors[:fname].should include('is required')
+    user.errors[:lname].should include('is required')
+  end
+
+  it 'assigns a unique referral code' do
+    user = User.create!(email: 'test@example.com', fname: 'John', lname: 'Doe')
+    expect(user.referral_code).to_not be_nil
+  end
+
+  # it 'sends a welcome email' do
+  #   user = User.create(email: 'test@example.com', fname: 'John', lname: 'Doe')
+  #   expect(Delayed::Job.count).to eq(1)
+  # end
+
+  context 'orders' do
+    before(:all) do
+      @user = User.create(email: 'test@example.com', fname: 'John', lname: 'Doe')
+      @product = Product.create(name: 'Rails for Dummies')
+    end
+
+    before(:each) do
+      @orders = []
+      @packages = []
+
+      10.times do
+        order = Order.create(user: @user, @product: @product)
+        package = Package.create(order: order, shipping_address: '123 Street Ave, New York, NY')
+        @orders << order
+        @packages << package
+      end
+    end
+
+    it 'returns a orders and packages for a user' do
+      expect(@user.orders.size).to eq(10)
+      expect(@user.packages.size).to eq(10)
+    end
+
+    context '#most_recent_package' do
+      it 'returns the most recent package' do
+        expect(@user.most_recent_package).to eq(@packages.last)
+      end
+
+      it 'the most recent package belongs to the last order' do
+        expect(@user.most_recent_package.order).to eq(@orders.last)
+      end
+    end
+
+    it 'the has_orders method indicates whether a user has any orders' do
+      expect(@user.has_orders).to eq(true)
+    end
+  end
+end
